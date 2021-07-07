3a30604d8143c7545c201023f5bfc14736c8628a
Peter Abbondanzo <peter@abbondanzo.com>
2021-07-07 00:01:55 -0400

    Initialize repository (#1)
    * Create nuxt project
    
    * Remove tutorial
    
    * Add typescript to fix linting issues
    
    Co-authored-by: Peter Abbondanzo <pabbondanzo@hubspot.com>

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+  </el-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
