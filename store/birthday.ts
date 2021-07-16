1a7418e6daeb854f4c4315910ac712f4d0b57888
Abbondanzo <peter@abbondanzo.com>
2021-07-15 23:28:46 -0400

    Use default birthday and have compute for current week

--- store/birthday.ts
+++ store/birthday.ts
@@ -1,4 +1,4 @@
-import { mutationTree } from 'typed-vuex'
+import { getterTree, mutationTree } from 'typed-vuex'
 
 interface Store {
   date: Date | null
@@ -8,7 +8,25 @@ interface SetPayload {
   date: Date
 }
 
-export const state = (): Store => ({ date: null })
+// May 21, 1998
+const tmp = new Date(1998, 4, 21)
+
+export const state = (): Store => ({
+  date: tmp, // null
+})
+
+const ONE_DAY = 1000 * 60 * 60 * 24
+
+export const getters = getterTree(state, {
+  currentWeek: (state) => {
+    const birthdate = state.date
+    if (!birthdate) return null
+    const nowEpoch = new Date().getTime()
+    const birthdayEpoch = birthdate.getTime()
+    const daysBetween = Math.round((nowEpoch - birthdayEpoch) / ONE_DAY)
+    return Math.ceil(daysBetween / 7)
+  },
+})
 
 export const mutations = mutationTree(state, {
   set(state, { date }: SetPayload) {
